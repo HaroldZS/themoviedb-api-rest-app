2432b748aae3a22de006f9c75d1cf4de73612f82
haroldZS <haroldzs1024@gmail.com>
2023-12-13 15:46:02 -0400

    Search movies by name

--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,9 @@
-searchFormBtn.addEventListener("click", () => (location.hash = "#search="));
+searchFormBtn.addEventListener(
+  "click",
+  () => (location.hash = `#search=${searchFormInput.value}`)
+);
 trendingBtn.addEventListener("click", () => (location.hash = "#trends"));
-arrowBtn.addEventListener("click", () => (location.hash = "#home"));
+arrowBtn.addEventListener("click", () => history.back());
 
 window.addEventListener("DOMContentLoaded", navigator, false);
 window.addEventListener("hashchange", navigator, false);
@@ -44,13 +47,16 @@ function searchPage() {
   arrowBtn.classList.remove("inactive");
   arrowBtn.classList.remove("header-arrow--white");
   headerTitle.classList.add("inactive");
-  headerCategoryTitle.classList.remove("inactive");
+  headerCategoryTitle.classList.add("inactive");
   searchForm.classList.remove("inactive");
 
   trendingPreviewSection.classList.add("inactive");
   categoriesPreviewSection.classList.add("inactive");
   genericSection.classList.remove("inactive");
   movieDetailSection.classList.add("inactive");
+
+  const [_, query] = location.hash.split("=");
+  getMoviesBySearch(query);
 }
 
 function movieDetailsPage() {
