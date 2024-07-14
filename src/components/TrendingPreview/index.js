a054beefdc84ac03d6a0b311ddcf8c79e4c9775c
haroldZS <haroldzs1024@gmail.com>
2024-07-14 12:22:05 -0400

    feat: Implement lazy loading on movies trending preview

--- src/components/TrendingPreview/index.js
+++ src/components/TrendingPreview/index.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TrendingPreview.css";
 import { useNavigate } from "react-router-dom";
 
 function TrendingPreview({ movies, likeMovie, likedMovies }) {
   const navigate = useNavigate();
   const likedMoviesIds = likedMovies.map((movie) => movie.id);
+  const imgRefs = useRef([]);
+
+  useEffect(() => {
+    const lazyLoader = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const url = img.getAttribute("data-src");
+          img.setAttribute("src", url);
+          lazyLoader.unobserve(img);
+        }
+      });
+    });
+
+    imgRefs.current.forEach((img) => {
+      if (img) {
+        lazyLoader.observe(img);
+      }
+    });
+
+    return () => {
+      imgRefs.current.forEach((img) => {
+        if (img) {
+          lazyLoader.unobserve(img);
+        }
+      });
+    };
+  }, [movies]);
 
   return (
     <section id="trendingPreview" className="trendingPreview-container">
@@ -21,17 +49,17 @@ function TrendingPreview({ movies, likeMovie, likedMovies }) {
       <article className="trendingPreview-movieList">
         {movies ? (
           <>
-            {movies.map((movie) => (
+            {movies.map((movie, index) => (
               <div
                 className="movie-container"
                 key={movie.id}
                 onClick={() => navigate(`/movies/${movie.id}`)}
               >
                 <img
-                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                  data-src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
                   className="movie-img"
                   alt={movie.title}
-                  id={`id${movie.id}`}
+                  ref={(el) => (imgRefs.current[index] = el)}
                 />
                 <button
                   className={`movie-btn ${
